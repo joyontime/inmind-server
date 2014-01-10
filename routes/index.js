d4f02891e3750e3558f3f470e3ea19c1ee94eb22
Joy C Chen <joyc@mit.edu>
2014-01-10 10:07:14 -0500

    Self signed SSL certs. Switch to HTTPS.

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,11 @@
  * GET home page.
  */
 exports.home = function(req, res){
-  res.send("Welcome to InMind.");
-  };
+  res.send(["Welcome to InMind.",
+            "<br>",
+            "Secure?",
+            req.secure,
+            "<br>",
+            "You are connecting to:",
+            req.get('Host')].join(' '));
+};
