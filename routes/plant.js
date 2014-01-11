4f68743f895e0a117a0da5d1077723f7ab724987
joy4luck <drgnfnix@gmail.com>
2014-01-11 12:38:27 -0500

    Plant calls, post, get.

--- routes/plant.js
+++ routes/plant.js
@@ -9,33 +9,36 @@ exports.home = function(req, res){
   res.send("Looking for plants?");
   };
 
-
-exports.get_all_plants = function(req, res){
-    plantProvider.findAll(function(error, docs){
-      res.json(docs);
-    });
-  };
-
+/**
+ * Args:
+ *  user
+ *  pinged_at
+ * Returns:
+ *  List of new plants
+ */
 exports.get_plants = function(req, res){
-    plantProvider.checkPlants(
-      req.param('user_id'),
-      req.param('pinged_at'),
-      function(error, plants){
-        res.json(plants);
-      }
-    );
-  };
-
-exports.get_plant_by_id = function(req, res) {
-    plantProvider.findById(req.params.id, function(error, plant) {
-      res.json(plant);
-    });
-  };
+  var pinged_at = new Date(+req.query.pinged_at);
+  console.log([req.user.user_id,
+    "( is lead:",
+    req.user.is_lead,
+    ") asked for plants.",
+    "Since:", pinged_at].join(' '));
+  plantProvider.checkPlants(
+    req.user.user_id,
+    pinged_at,
+    function(error, plants){
+      res.json(plants);
+    }
+  );
+};
 
 exports.post_plant = function(req, res){
+  if (!req.user.is_lead){
+    res.json("I'm sorry, you need to have gardener status.");
+  } else {
     plantProvider.save({
-      group_id: req.param('group_id'),
-      shared_with: req.param('shared_with'),
+      group_id: req.user.group_id,
+      shared_with: JSON.parse(req.param('shared_with')),
       title: req.param('title'),
       salt: req.param('salt'),
       passphrase: req.param('passphrase'),
@@ -43,7 +46,8 @@ exports.post_plant = function(req, res){
         res.json(plant);
       }
     );
-  };
+  }
+};
 
 exports.update_plant = function(req, res){
     plantProvider.updatePlant(req.params.id,
@@ -52,7 +56,3 @@ exports.update_plant = function(req, res){
         res.json("Success!");
     });
   };
-
-
-
-
--- scripts/post_plant.sh
+++ scripts/post_plant.sh
@@ -1,12 +1,10 @@
-export GROUP="group_id=1"
-export SHARED_WITH="shared_with=['joy4luck', 'foo']"
-export TITLE="title=gardenia"
+export SHARED_WITH='shared_with=["joy4luck"]'
+export TITLE="title=iris"
 export SALT="salt=NaCl"
 export PASS="passphrase=open"
 
 curl \
 -k -u joy4luck:boo \
---data-urlencode "$GROUP" \
 --data-urlencode "$SHARED_WITH" \
 --data-urlencode "$TITLE" \
 --data-urlencode "$SALT" \
