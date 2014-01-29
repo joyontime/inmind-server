168e2f9ff0f1d083a7613489fe5defadaa33c69d
joy4luck <joyc@mit.edu>
2014-01-29 16:04:08 -0500

    Add lead user mongo script.

---
+++ scripts/mongo_lead_user.js
@@ -0,0 +1,13 @@
+db.users.insert({
+"group_id" : "1",
+"alias" : "Joy",
+"user_id" : "joy4luck",
+"passphrase" : "boo",
+"is_lead" : true,
+"created_at" : ISODate()});
+
+db.groups.insert({
+"IV" : "c60bddc14132d527ff7baa103bce0321",
+"created_at" : ISODate(),
+"group_id" : "1",
+"members" : ["" ]});
